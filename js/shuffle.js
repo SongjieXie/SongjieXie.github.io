fbb364c5d2a747e8c22af0db038140fc24584868
Songjie Xie <xiesj@shanghaitech.edu.cn>
2023-07-29 10:31:48 +0800

    Update shuffle.js

--- js/shuffle.js
+++ js/shuffle.js
@@ -1,5 +1,5 @@
 function shuffle_stanford() {
     shuffleLetters(document.getElementById('my_email'), {
-        text: 'xiesj@shanghaitech.edu.cn',
+        text: 'sxieat@connect.ust.hk',
     });
 }
